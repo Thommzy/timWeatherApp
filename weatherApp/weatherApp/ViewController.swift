cd723044886def9aed82903dae7dcf6619d603a4
Thommzy <tomiwaobeisun@yahoo.com>
2020-09-13 14:15:52 +0100

    created the new xcode Project and i added the icons and Images needed

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Tim on 13/09/2020.
+//  Copyright © 2020 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
