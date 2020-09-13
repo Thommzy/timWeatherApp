9c130a5a27ea97eabe5833043712a3236def6ca4
Thommzy <tomiwaobeisun@yahoo.com>
2020-09-13 22:36:26 +0100

    The storyboard design done

---
+++ weatherApp/weatherApp/WeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.swift
+//  weatherApp
+//
+//  Created by Tim on 13/09/2020.
+//  Copyright © 2020 Tim. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
